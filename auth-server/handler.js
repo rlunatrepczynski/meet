67300197334ea8ec9f7bb1aae7eeacd94db57ac1
crene <crenee72@hotmail.com>
2024-03-10 15:51:13 -0700

    Update handler.js
    Updated typo

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -57,7 +57,7 @@ module.exports.getAccessToken = async (event) => {
       return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origins': '*',
+          'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Credentials': true,
         },
         body: JSON.stringify(results),
