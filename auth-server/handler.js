8416724b49ed89cab2d43cf8f832576411c5b1f2
crene <crenee72@hotmail.com>
2024-03-11 22:37:23 -0700

    Update handler.js
    Updated handler.js with calendar events

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -72,3 +72,45 @@ module.exports.getAccessToken = async (event) => {
       };
     });
 };
+
+module.exports.getCalendarEvents = async (event) => {
+  // Decode authorization code extracted from the Url query
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(response);
+      }
+    );
+  })
+    .then((results) => {
+      // Respond with oAuth token
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+    .catch((error) => {
+      // Handle Error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
