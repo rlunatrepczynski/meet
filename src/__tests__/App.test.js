74f4491655f67e24a37d58cca4cced8b32746461
crene <crenee72@hotmail.com>
2024-03-14 21:53:47 -0700

    Update App.test.js
    Added describe() for App.test.js

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,9 @@
+// src/__tests__/App.test.js
+
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('renders list of events', () => {
+    const AppDOM = render(<App />).container.firstChild;
+    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+});
