dc1e4d06d887fac216428d2f3c2cb0da6b06f431
crene <crenee72@hotmail.com>
2024-03-15 20:37:15 -0700

    Update CitySearch.test.js
    Correct Error

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -4,7 +4,7 @@ import CitySearch from '../components/CitySearch';
 describe('<CitySearch /> component', () => {
     test('renders text input', () => {
         const CitySearchComponent = render(<CitySearch />);
-        const cityTextBox = CitySearchComponent.queryAllByRole('textbox');
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
         expect(cityTextBox).toBeInTheDocument();
         expect(cityTextBox).toHaveClass('city');
     });
