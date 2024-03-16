dc40ff8da527f66cd32cc72e1bec92df1dd8e5b2
crene <crenee72@hotmail.com>
2024-03-16 00:31:24 -0700

    Update EventList.js
    Changed from double quotes to single quotes to be better consistent with my code

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,12 +1,10 @@
-import Event from "./Event";
+import Event from './Event';
 
 const EventList = ({ events }) => {
     return (
-        <ul id="event-list">
-            {events ?
-                events.map(event => <Event key={event.id} event={event} />)
-                : null}
-        </ul>
+      <ul id="event-list">
+        {events? events.map(event => <Event key={event.id} event={event} />): null}
+      </ul>
     );
 }
 
