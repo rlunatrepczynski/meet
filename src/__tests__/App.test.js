ceeee7e1bee3bddad472af2f3f96a2e28cae60bc
crene <crenee72@hotmail.com>
2024-03-15 17:16:05 -0700

    Update App.test.js
    Adjusted the indentation from 4 to 2

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,6 +4,6 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 test('renders list of events', () => {
-    const AppDOM = render(<App />).container.firstChild;
-    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+  const AppDOM = render(<App />).container.firstChild;
+  expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
 });
