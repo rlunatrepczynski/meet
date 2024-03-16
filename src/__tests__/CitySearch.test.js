5e78dbf45850828ac3c59663cac5cc08c179fd6b
crene <crenee72@hotmail.com>
2024-03-15 20:59:36 -0700

    Update CitySearch.test.js
    Updated two add two more tests(suggestions list is hidden by default and renders a list of suggestions when city textbox gains focus).

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
 
 describe('<CitySearch /> component', () => {
@@ -7,5 +8,22 @@ describe('<CitySearch /> component', () => {
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
         expect(cityTextBox).toBeInTheDocument();
         expect(cityTextBox).toHaveClass('city');
+    });
+
+    test('suggestions list is hidden by default', () => {
+        const CitySearchComponent = render(<CitySearch />);
+        const suggestionList = CitySearchComponent.queryByRole('list');
+        expect(suggestionList).not.toBeInTheDocument();
+    });
+
+    test('renders a list of suggestions when city textbox gains focus', 
+    async () => {
+        const CitySearchComponent= render(<CitySearch />);
+        const user = userEvent.setup();
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.click(cityTextBox);
+        const suggestionList = CitySearchComponent.queryByRole('list');
+        expect(suggestionList).toBeInTheDocument();
+        expect(suggestionList).toHaveClass('suggestions');
     });
 });
