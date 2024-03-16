8803cda9d661ef112e0726665427d3fb1dc95a6c
crene <crenee72@hotmail.com>
2024-03-15 18:15:20 -0700

    Update EventList.test.js
    Added test for renders correct number of events

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -6,4 +6,10 @@ describe('<EventList /> component', () => {
     const EventListComponent = render(<EventList />);
     expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
   });
+
+  test('renders correct number of events', () => {
+    const EventListComponent = render(<EventList events={[{}, {}, {}, {}]} />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+  });
 });
+
