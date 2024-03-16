267354010cd49b7753277453e99961b2255bfd1a
crene <crenee72@hotmail.com>
2024-03-15 18:26:05 -0700

    Update EventList.test.js
    Added id to events

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -8,7 +8,7 @@ describe('<EventList /> component', () => {
   });
 
   test('renders correct number of events', () => {
-    const EventListComponent = render(<EventList events={[{}, {}, {}, {}]} />);
+    const EventListComponent = render(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
     expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
   });
 });
