e7e63c6334ebae3c93f5e6af96340c13da0d0e6c
crene <crenee72@hotmail.com>
2024-03-18 22:16:17 -0700

    Update App.js
    Created new state for allLocations and initialized allLocations in fetch data.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
-import { getEvents } from './api';
+import { useEffect, useState } from 'react';
+import { extractLocations, getEvents } from './api';
 
 import './App.css';
 
-function App() {
+const App = () => {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const fetchData = async () => {
     const allEvents = await getEvents();
     setEvents(allEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []); // Empty dependency array means this effect runs once on mount
 
   return (
     <div className="App">
-      <CitySearch />
+      <CitySearch allLocations={allLocations} />
       <NumberOfEvents />
       <EventList events={events} />
     </div>
