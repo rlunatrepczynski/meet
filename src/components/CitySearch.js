f7fc513ac779b2452428361f7217178d6fead4cc
crene <crenee72@hotmail.com>
2024-03-21 22:47:20 -0700

    Update CitySearch.js
    Moved useEffect

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -5,10 +5,6 @@ const CitySearch = ({ allLocations, setCurrentCity }) => {
     const [query, setQuery] = useState("");
     const [suggestions, setSuggestions] = useState([]);
 
-    useEffect(() => {
-        setSuggestions(allLocations);
-    }, [`${allLocations}`]);
-
     const handleInputChanged = (event) => {
         const value = event.target.value;
         const filteredLocations = allLocations ? allLocations.filter((location) => {
@@ -23,6 +19,11 @@ const CitySearch = ({ allLocations, setCurrentCity }) => {
         setShowSuggestions(false); // to hide the list
         setCurrentCity(value);
     };
+
+    useEffect(() => {
+        setSuggestions(allLocations);
+    }, [`${allLocations}`]);
+
     return (
         <div id="city-search">
             <input
