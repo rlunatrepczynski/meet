6bd51456829a6925e1c3269b4069afb539c81d69
crene <crenee72@hotmail.com>
2024-03-24 21:53:04 -0700

    Update filterEventsByCity.test.js
    Added AppComponent and await waitFor().

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,4 +1,6 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import { render, waitFor, within } from '@testing-library/react';
+import App from '../App';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
@@ -8,11 +10,19 @@ defineFeature(feature, test => {
 
         });
 
+        let AppComponent;
         when('the user opens the app.', () => {
-
+            AppComponent = render(<App />);
         });
 
-        then('the user should see a list of upcoming events.', () => {
+        then('the user should see a list of upcoming events.', async () => {
+            const AppDOM = AppComponent.container.firstChild;
+            const EventListDOM = AppDOM.querySelector('#event-list');
+
+            await waitFor(() => {
+                const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+                expect(EventListItems.length).toBe(32);
+            });
 
         });
     });
