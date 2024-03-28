874ed293619638ee3152f1b7509680e85d4e9603
crene <crenee72@hotmail.com>
2024-03-28 01:06:10 -0700

    Update index.js
    Imported atatus and added initialization/config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('96982a1b555541faa393b8a1fbef51d8').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
