c735aeae716a9f2218b32e5e5fc0467584f362c6
crene <crenee72@hotmail.com>
2024-03-31 22:53:24 -0700

    Update CitySearch.test.js
    Added setInfoAlert to render and rerender for CitySearch

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -7,7 +7,10 @@ import { extractLocations, getEvents } from '../api';
 describe('<CitySearch /> component', () => {
     let CitySearchComponent;
     beforeEach(() => {
-        CitySearchComponent = render(<CitySearch allLocations={[]} />);
+        CitySearchComponent = render(<CitySearch allLocations={[]}
+            setCurrentCity={() => { }}
+            setInfoAlert={() => { }}
+        />);
     });
     test('renders text input', () => {
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -31,7 +34,10 @@ describe('<CitySearch /> component', () => {
         const user = userEvent.setup();
         const allEvents = await getEvents();
         const allLocations = extractLocations(allEvents);
-        CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+        CitySearchComponent.rerender(<CitySearch allLocations={allLocations}
+            setCurrentCity={() => { }}
+            setInfoAlert={() => { }}
+        />);
 
         // user types "Berlin" in city textbox
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -56,6 +62,7 @@ describe('<CitySearch /> component', () => {
         CitySearchComponent.rerender(<CitySearch
             allLocations={allLocations}
             setCurrentCity={() => { }}
+            setInfoAlert={() => { }}
         />);
 
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
