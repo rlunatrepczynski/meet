b18c220df46a2a845c1d2081f6e3c18053651fa7
crene <crenee72@hotmail.com>
2024-04-01 18:01:18 -0700

    Update App.js
    Added error alert to import, useState and to className "alert-containers".

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
-import { InfoAlert } from './components/Alert';
+import { InfoAlert, ErrorAlert } from './components/Alert';
 
 import './App.css';
 
@@ -13,6 +13,7 @@ const App = () => {
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
 
   const fetchData = async () => {
     const allEvents = await getEvents();
@@ -36,12 +37,14 @@ const App = () => {
     <div className="App">
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
       </div>
-      <CitySearch 
-      allLocations={allLocations} 
-      setCurrentCity={setCurrentCity}
-      setInfoAlert={setInfoAlert} />
-      <NumberOfEvents setNumberOfEvents={handleNumberOfEventsChange} />
+      <CitySearch
+        allLocations={allLocations}
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert} />
+      <NumberOfEvents setNumberOfEvents={handleNumberOfEventsChange}
+        setErrorAlert={setErrorAlert} />
       <EventList events={events} />
     </div>
   );
