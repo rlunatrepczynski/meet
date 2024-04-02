76cee6f253c2cc31fa4f4b5fafdcd383bd5b5c6e
crene <crenee72@hotmail.com>
2024-04-01 18:00:27 -0700

    Update NumberOfEvents.test.js
    Added error alert for NumberofEventsComponent

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -5,7 +5,8 @@ import userEvent from '@testing-library/user-event';
 describe('<NumberOfEvents /> component', () => {
     let NumberOfEventsComponent;
     beforeEach(() => {
-        NumberOfEventsComponent = render(<NumberOfEvents setNumberOfEvents={() => {}} />);
+        NumberOfEventsComponent = render(<NumberOfEvents setNumberOfEvents={() => { }}
+            setErrorAlert={() => { }} />);
     });
     test('has an element with "textbox" role', () => {
         expect(NumberOfEventsComponent.queryByRole('textbox')).toBeInTheDocument();
@@ -13,7 +14,7 @@ describe('<NumberOfEvents /> component', () => {
     test('default value of the input field is 32', () => {
         expect(NumberOfEventsComponent.queryByRole('textbox')).toHaveValue('32');
     });
-    test('value changes accordingly when user types', async() => {
+    test('value changes accordingly when user types', async () => {
         const NumberOfEvents = NumberOfEventsComponent.queryByRole('textbox');
         const user = userEvent.setup();
         await user.type(NumberOfEvents, '{backspace}{backspace}10');
