85250e7fd64cd768a9cd243275f8e233cf015320
crene <crenee72@hotmail.com>
2024-04-05 22:27:29 -0700

    Update api.js
    Updated api.js for getEvents

--- src/api.js
+++ src/api.js
@@ -17,9 +17,12 @@ export const extractLocations = (events) => {
  * This function will fetch the list of all events
  */
 export const getEvents = async () => {
+    if (window.location.href.startsWith('http://localhost')) {
+        return mockData;
+    }
+
     if (!navigator.onLine) {
         const events = localStorage.getItem("lastEvents");
-        NProgress.done();
         return events ? JSON.parse(events) : [];
     }
 
